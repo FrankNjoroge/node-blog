04cc48133bda50733d6e360e218c489138af3b33
Frank Njoroge <franknjoroge@Franks-MacBook-Pro.local>
2023-04-14 22:37:17 +0300

    added view engine

--- app.js
+++ app.js
@@ -2,25 +2,29 @@ const express = require("express");
 
 const app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  // res.send("<h1>Semaje budaaaa</h1>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    { title: "Yoshi finds eggs", snippet: "lorem ipsum dolor sit amet" },
+    { title: "Mario finds stars", snippet: "lorem ipsum dolor sit amet" },
+    { title: "How to win", snippet: "lorem ipsum dolor sit amet" },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  // res.send("<h1>Semaje budaaaa</h1>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "Aboutus" });
 });
 
-//redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create New" });
 });
 
 //404
 app.use((req, res) => {
-  res.statusCode = 404;
-  res.sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: 404 });
 });
